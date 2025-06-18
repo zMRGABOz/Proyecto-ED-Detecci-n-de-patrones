1e89ce15cc5e2798d7af86067a56662ad0ed60c9
marCRACK29 <mamartinez2023@udec.cl>
2025-06-17 22:55:59 -0400

    feat: agrega función para listar archivos pasando rutas absolutas

--- src/algoritmos/boyer_moore/main_boyer_moore.cpp
+++ src/algoritmos/boyer_moore/main_boyer_moore.cpp
@@ -1,8 +1,16 @@
 #include "boyer_moore.h"
+#include "../../listar_archivos.h"
+
+// compilar con: 
+// g++ -o boyer_moore main_boyer_moore.cpp boyer_moore.cpp ../../listar_archivos.cpp -I../../
+// ./boyer_moore
 
 int main() {
     string txt = "ABAAABCDABAA";
     string pat = "ABAA";
     search(txt, pat);
+
+    listarArchivos();
+
     return 0;
 }
