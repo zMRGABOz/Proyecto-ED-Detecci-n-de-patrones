5c265b169a179fac30c051080ed0a7b18057c2a6
marCRACK29 <mamartinez2023@udec.cl>
2025-06-20 16:38:13 -0400

    añade cantidad límite de archivos en load_documents

--- src/leer_archivos.h
+++ src/leer_archivos.h
@@ -15,10 +15,11 @@ struct document {
 };
 
 /**
- * @brief función para cargar documentos y su contenido desde un directorio.
+ * @brief función para cargar documentos y su contenido desde un directorio
  * 
- * @param directory_path ruta hacia el directorio.
+ * @param directory_path ruta hacia el directorio
+ * @param num_files cantidad de archivos a leer
  */
-vector<document> load_documents(const string& directory_path);
+vector<document> load_documents(const string& directory_path, int num_files);
 
 #endif
