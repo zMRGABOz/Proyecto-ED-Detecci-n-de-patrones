ce2d83fbb1b0d487009b078799152e2d0c731e03
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-06-30 17:23:39 -0400

    Codigo para precomputar SuffixArrayOriginal

--- src/estructuras_de_datos/SuffixArrayOriginal.cpp
+++ src/estructuras_de_datos/SuffixArrayOriginal.cpp
@@ -38,13 +38,21 @@ vector<int> build_suffix_array(string &texto) {
 }
 
 
-string concatenar_textos(const vector<string>& textos) {
+string concatenar_textos(vector<document>& documentos) {
     string resultado;
-    //guarda a que texto pertenece cada caracter, y lo escribe en el resultado
-    for(string s : textos){
-        resultado += s;
-        resultado += "$";
+    int posicion_actual = 0;
+
+    for (auto& doc : documentos) {
+        doc.start_index = posicion_actual;
+        resultado += doc.text;
+        posicion_actual += doc.text.size();
+
+        resultado += "$"; // delimitador
+        posicion_actual += 1;
+
+        doc.end_index = posicion_actual; // posición justo después del $
     }
+
     return resultado;
 }
 
@@ -109,38 +117,210 @@ map<int, int> contar_ocurrencias_por_texto(
 }
 
 
-int main() {
-    vector<string> textos = {
-        "la casa azul",
-        "otra flor roja",
-        "el perro duerme en la casa",
-        "una flor roja y otra flor rojae"
-    };
+
+int main(){
+    
+    string ruta_documentos = "../../datos_de_pruebas/documentos/DNA";
+
+    for (int i = 5; i <= 5; i += 5) {
+        vector<document> docs = cargarDocumentos(ruta_documentos, i);
+        string texto_total = concatenar_textos(docs); // se concatenan los documentos
+
+        auto crear_sa_start = chrono::high_resolution_clock::now();
+        vector<int> suffix_array = build_suffix_array(texto_total);
+        auto crear_sa_end = chrono::high_resolution_clock::now();
+
+        double crear_sa_time = chrono::duration_cast<chrono::nanoseconds>(crear_sa_end - crear_sa_start).count();
+        cout << "Tiempo creacion SA con " << i << " documentos: " << crear_sa_time << " segundos" << endl;
+
+        // Nombre del archivo binario: DNA_5.bin, DNA_10.bin, etc.
+        string tipo_documento = "DNA"; // Puedes ajustarlo dinámicamente si lo necesitas
+        string nombre_archivo = "SA_Precompilados/" + tipo_documento + "_" + to_string(i) + ".bin";
+
+        // Abrir archivo en modo binario
+        ofstream archivo_bin(nombre_archivo, ios::binary);
+        if (!archivo_bin) {
+            cerr << "Error al abrir el archivo " << nombre_archivo << " para escritura.\n";
+            continue;
+        }
+
+        int size = suffix_array.size();
+
+        // Guardar: tamaño, tiempo y datos del suffix array
+        archivo_bin.write(reinterpret_cast<char*>(&size), sizeof(int));          // Cantidad de sufijos
+        archivo_bin.write(reinterpret_cast<char*>(&crear_sa_time), sizeof(double)); // Tiempo en segundos
+        archivo_bin.write(reinterpret_cast<char*>(suffix_array.data()), size * sizeof(int)); // Datos del SA
+
+        archivo_bin.close();
+    }
+
+    ruta_documentos = "../../datos_de_pruebas/documentos/English";
+
+    for (int i = 1; i <= 40; i += 5) {
+        vector<document> docs = cargarDocumentos(ruta_documentos, i);
+        string texto_total = concatenar_textos(docs); // se concatenan los documentos
+
+        auto crear_sa_start = chrono::high_resolution_clock::now();
+        vector<int> suffix_array = build_suffix_array(texto_total);
+        auto crear_sa_end = chrono::high_resolution_clock::now();
+
+        double crear_sa_time = chrono::duration_cast<chrono::nanoseconds>(crear_sa_end - crear_sa_start).count();
+        cout << "Tiempo creacion SA con " << i << " documentos: " << crear_sa_time << " segundos" << endl;
+
+        // Nombre del archivo binario: DNA_5.bin, DNA_10.bin, etc.
+        string tipo_documento = "English"; // Puedes ajustarlo dinámicamente si lo necesitas
+        string nombre_archivo = "SA_Precompilados/" + tipo_documento + "_" + to_string(i) + ".bin";
+
+        // Abrir archivo en modo binario
+        ofstream archivo_bin(nombre_archivo, ios::binary);
+        if (!archivo_bin) {
+            cerr << "Error al abrir el archivo " << nombre_archivo << " para escritura.\n";
+            continue;
+        }
+
+        int size = suffix_array.size();
+
+        // Guardar: tamaño, tiempo y datos del suffix array
+        archivo_bin.write(reinterpret_cast<char*>(&size), sizeof(int));          // Cantidad de sufijos
+        archivo_bin.write(reinterpret_cast<char*>(&crear_sa_time), sizeof(double)); // Tiempo en segundos
+        archivo_bin.write(reinterpret_cast<char*>(suffix_array.data()), size * sizeof(int)); // Datos del SA
+
+        archivo_bin.close();
+    }
+
+
+    ruta_documentos = "../../datos_de_pruebas/documentos/Proteins";
+    
+    for (int i = 5; i <= 40; i += 5) {
+        vector<document> docs = cargarDocumentos(ruta_documentos, i);
+        string texto_total = concatenar_textos(docs); // se concatenan los documentos
+
+        auto crear_sa_start = chrono::high_resolution_clock::now();
+        vector<int> suffix_array = build_suffix_array(texto_total);
+        auto crear_sa_end = chrono::high_resolution_clock::now();
+
+        double crear_sa_time = chrono::duration_cast<chrono::nanoseconds>(crear_sa_end - crear_sa_start).count();
+        cout << "Tiempo creacion SA con " << i << " documentos: " << crear_sa_time << " segundos" << endl;
+
+        // Nombre del archivo binario: DNA_5.bin, DNA_10.bin, etc.
+        string tipo_documento = "Proteins"; // Puedes ajustarlo dinámicamente si lo necesitas
+        string nombre_archivo = "SA_Precompilados/" + tipo_documento + "_" + to_string(i) + ".bin";
+
+        // Abrir archivo en modo binario
+        ofstream archivo_bin(nombre_archivo, ios::binary);
+        if (!archivo_bin) {
+            cerr << "Error al abrir el archivo " << nombre_archivo << " para escritura.\n";
+            continue;
+        }
+
+        int size = suffix_array.size();
+
+        // Guardar: tamaño, tiempo y datos del suffix array
+        archivo_bin.write(reinterpret_cast<char*>(&size), sizeof(int));          // Cantidad de sufijos
+        archivo_bin.write(reinterpret_cast<char*>(&crear_sa_time), sizeof(double)); // Tiempo en segundos
+        archivo_bin.write(reinterpret_cast<char*>(suffix_array.data()), size * sizeof(int)); // Datos del SA
+
+        archivo_bin.close();
+    }
+
+    ruta_documentos = "../../datos_de_pruebas/documentos/Sources";
+
+    for (int i = 5; i <= 40; i += 5) {
+        vector<document> docs = cargarDocumentos(ruta_documentos, i);
+        string texto_total = concatenar_textos(docs); // se concatenan los documentos
+
+        auto crear_sa_start = chrono::high_resolution_clock::now();
+        vector<int> suffix_array = build_suffix_array(texto_total);
+        auto crear_sa_end = chrono::high_resolution_clock::now();
+
+        double crear_sa_time = chrono::duration_cast<chrono::nanoseconds>(crear_sa_end - crear_sa_start).count();
+        cout << "Tiempo creacion SA con " << i << " documentos: " << crear_sa_time << " segundos" << endl;
+
+        // Nombre del archivo binario: DNA_5.bin, DNA_10.bin, etc.
+        string tipo_documento = "Sources"; // Puedes ajustarlo dinámicamente si lo necesitas
+        string nombre_archivo = "SA_Precompilados/" + tipo_documento + "_" + to_string(i) + ".bin";
+
+        // Abrir archivo en modo binario
+        ofstream archivo_bin(nombre_archivo, ios::binary);
+        if (!archivo_bin) {
+            cerr << "Error al abrir el archivo " << nombre_archivo << " para escritura.\n";
+            continue;
+        }
+
+        int size = suffix_array.size();
+
+        // Guardar: tamaño, tiempo y datos del suffix array
+        archivo_bin.write(reinterpret_cast<char*>(&size), sizeof(int));          // Cantidad de sufijos
+        archivo_bin.write(reinterpret_cast<char*>(&crear_sa_time), sizeof(double)); // Tiempo en segundos
+        archivo_bin.write(reinterpret_cast<char*>(suffix_array.data()), size * sizeof(int)); // Datos del SA
+
+        archivo_bin.close();
+    }
+
+}
+
+
+//g++ .\SuffixArrayOriginal.cpp  ../leer_archivos.cpp -o SuffixArrayOriginal
+//./SuffixArrayOriginal
+
+
+//codigo para generar suffix array  precomputados
+/*
+int main(int argc,  char** argv) {
+    
+    if(argc < 3){ 
+       cerr << "Uso: " << argv[0] << " <ruta a patron> <numero de archivos>" << endl;
+       return 1;
+    }
+    string ruta_documentos = "../../datos_de_pruebas/documentos/DNA";
+    string ruta_patron = argv[1];
+    int cant_documentos = atoi(argv[2]);
+    vector<document> docs = cargarDocumentos(ruta_documentos, cant_documentos);
+    vector<string> patrones = cargarPatrones(ruta_patron);
+
+
     vector<int> largos_textos;
     int acumulado = 0;
-    for (const string& s : textos) {
-        acumulado += s.length() + 1;  // +1 por el '$'
+
+    for (const auto& d : docs) {
+        acumulado += d.text.length() + 1;  // +1 por el '$'
         largos_textos.push_back(acumulado);
     }
-    string texto_total = concatenar_textos(textos);
+
+    string texto_total = concatenar_textos(docs);
+    auto crear_sa_start = chrono::high_resolution_clock::now();
     vector<int> suffix_array = build_suffix_array(texto_total);
+    auto crear_sa_end = chrono::high_resolution_clock::now();
 
-    string subfrase = "e";
+    double crear_sa_time = chrono::duration_cast<chrono::nanoseconds>(crear_sa_end - crear_sa_start).count();
+    cout << crear_sa_time * 1e-9 << endl;
+    cout << "lol" << endl;
+    map<int,int> resultados;
+    for(string s : patrones){
+        string patron = s;
+        
+        auto encontrar_sa_start = chrono::high_resolution_clock::now();
+        resultados = contar_ocurrencias_por_texto(
+        texto_total, suffix_array, patron, largos_textos
+        );
+        auto encontrar_sa_end = chrono::high_resolution_clock::now();
 
-    map<int, int> resultados = contar_ocurrencias_por_texto(
-    texto_total, suffix_array, subfrase, largos_textos
-    );
+        double encontrar_sa_time = chrono::duration_cast<chrono::nanoseconds>(encontrar_sa_end - encontrar_sa_start).count();
+        
+        cout << encontrar_sa_time * 1e-9 << endl;
 
-    if (!resultados.empty()) {
-        for (const auto& [id_texto, cantidad] : resultados) {
-            cout << "Subfrase encontrada " << cantidad << " vez" 
-                << (cantidad > 1 ? "es" : "") << " en texto #" << id_texto + 1 << "\n";
+        if (!resultados.empty()) {
+            for (const auto& [id_texto, cantidad] : resultados) {
+                cout << "Patron: " << patron <<" encontrado " << cantidad << " vez" 
+                    << (cantidad > 1 ? "es" : "") << " en texto #" << id_texto + 1 <<"\n";
+            }
+        } 
+
+        else {
+            cout << "Patron no encontrada.\n";
         }
-    } 
-    else {
-        cout << "Subfrase no encontrada.\n";
     }
-
 
     return 0;
 }
+*/
