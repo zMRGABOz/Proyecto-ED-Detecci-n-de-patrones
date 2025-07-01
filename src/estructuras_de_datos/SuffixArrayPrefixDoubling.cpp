3e73e851b25d6e8fb24d705dc19e2c5cd089cf80
Pvilla14 <pablo.villagran.hermanns@gmail.com>
2025-06-30 22:48:36 -0400

    SuffixArrayPrefixDoubling corregido

--- src/estructuras_de_datos/SuffixArrayPrefixDoubling.cpp
+++ src/estructuras_de_datos/SuffixArrayPrefixDoubling.cpp
@@ -175,7 +175,7 @@ map<int, int> contar_ocurrencias_por_texto(
 void Precomputar_suffix_arrays(){
     
     string ruta_documentos = "../../datos_de_pruebas/documentos/DNA";
-    for (int i = 5; i <= 5; i += 5) {
+    for (int i = 5; i <= 40; i += 5) {
         vector<document> docs = cargarDocumentos(ruta_documentos, i);
         string texto_total = concatenar_textos(docs); // se concatenan los documentos
 
@@ -311,75 +311,66 @@ void Precomputar_suffix_arrays(){
 //codigo para evaluar patrones con suffix arrays precomputados
 int main(int argc, char** argv) {
 
-    Precomputar_suffix_arrays();
-    /*
-    if (argc < 2) {
-        cerr << "Uso: " << argv[0] << " <ruta a archivo de patrones>" << endl;
+    //Precomputar_suffix_arrays();
+
+    if (argc < 3) {
+        cerr << "Uso: " << argv[0] << " <ruta a archivo de patrones; > "<< argv[1] << "<cant_documentos>" << endl;
         return 1;
     }
 
     string ruta_patrones = argv[1];
+    int cant_documentos = atoi(argv[2]);
     string ruta_documentos_base = "../../datos_de_pruebas/documentos/DNA"; //cambiar para probar con otros arhivos
     string tipo_documento = "DNA"; // Ajusta si usas otro dataset //cambiar para probar con otros archivos
 
     vector<string> patrones = cargarPatrones(ruta_patrones);
 
-    for (int cant_documentos = 5; cant_documentos <= 40; cant_documentos += 5) {
-        cout << "\n=== Evaluando con " << cant_documentos << " documentos ===\n";
 
-        // Cargar SA desde archivo binario
-        string nombre_archivo = "SA_PD_Precompilados/" + tipo_documento + "_" + to_string(cant_documentos) + ".bin";
-        ifstream archivo_bin(nombre_archivo, ios::binary);
-        if (!archivo_bin) {
-            cerr << "Error al abrir el archivo binario: " << nombre_archivo << endl;
-            continue;
-        }
-
-        int size = 0;
-        double tiempo_creacion = 0;
-        archivo_bin.read(reinterpret_cast<char*>(&size), sizeof(int));
-        archivo_bin.read(reinterpret_cast<char*>(&tiempo_creacion), sizeof(double));
-
-        vector<int> suffix_array(size);
-        archivo_bin.read(reinterpret_cast<char*>(suffix_array.data()), size * sizeof(int));
-        archivo_bin.close();
-
-        // Cargar documentos y concatenar texto
-        vector<document> docs = cargarDocumentos(ruta_documentos_base, cant_documentos);
-        vector<int> largos_textos;
-        int acumulado = 0;
-        for (const auto& d : docs) {
-            acumulado += d.text.length() + 1; // +1 por '$'
-            largos_textos.push_back(acumulado);
-        }
-
-        string texto_total = concatenar_textos(docs);
-
-        // Buscar todos los patrones
-        for (const string& patron : patrones) {
-            auto start = chrono::high_resolution_clock::now();
-            map<int, int> resultados = contar_ocurrencias_por_texto(
-                texto_total, suffix_array, patron, largos_textos
-            );
-            auto end = chrono::high_resolution_clock::now();
-
-            double tiempo_busqueda = chrono::duration_cast<chrono::nanoseconds>(
-                end - start
-            ).count() * 1e-9;
-
-            cout << "Tiempo búsqueda para patrón \"" << patron << "\": "
-                 << tiempo_busqueda << " segundos\n";
-
-            if (!resultados.empty()) {
-                for (const auto& [id_texto, cantidad] : resultados) {
-                    cout << "Patron \"" << patron << "\" encontrado " << cantidad
-                         << " vez" << (cantidad > 1 ? "es" : "") << " en texto #" << id_texto + 1 << "\n";
-                }
-            } else {
-                cout << "  → Patron \"" << patron << "\" no encontrado.\n";
-            }
-        }
+    // Cargar SA desde archivo binario
+    string nombre_archivo = "SA_PD_Precompilados/" + tipo_documento + "_" + to_string(cant_documentos) + ".bin";
+    ifstream archivo_bin(nombre_archivo, ios::binary);
+    if (!archivo_bin) {
+        cerr << "Error al abrir el archivo binario: " << nombre_archivo << endl;
     }
-*/
+
+    int size = 0;
+    double tiempo_creacion = 0;
+    archivo_bin.read(reinterpret_cast<char*>(&size), sizeof(int));
+    archivo_bin.read(reinterpret_cast<char*>(&tiempo_creacion), sizeof(double));
+
+    vector<int> suffix_array(size);
+    archivo_bin.read(reinterpret_cast<char*>(suffix_array.data()), size * sizeof(int));
+    archivo_bin.close();
+
+    // Cargar documentos y concatenar texto
+    vector<document> docs = cargarDocumentos(ruta_documentos_base, cant_documentos);
+    vector<int> largos_textos;
+    int acumulado = 0;
+    for (const auto& d : docs) {
+        acumulado += d.text.length() + 1; // +1 por '$'
+        largos_textos.push_back(acumulado);
+    }
+
+    string texto_total = concatenar_textos(docs);
+
+    //tiempo de busqueda todos los patrones
+    auto start = chrono::high_resolution_clock::now();
+    // Buscar todos los patrones
+    for (const string& patron : patrones) {
+        map<int, int> resultados = contar_ocurrencias_por_texto(
+            texto_total, suffix_array, patron, largos_textos
+        );
+    }
+    
+    auto end = chrono::high_resolution_clock::now();
+
+    double tiempo_busqueda = chrono::duration_cast<chrono::nanoseconds>(
+        end - start
+    ).count() * 1e-9;
+    
+    cout << "../src/estructuras_de_datos/SuffixArrayPrefixDoubling.cpp;" <<
+    patrones.size() << ";" << cant_documentos << ";" << tiempo_busqueda << ";" << endl;
+
+
     return 0;
 }
